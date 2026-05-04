74eef5cdc49b7b0b7865779beb863cae4ef7cded
Your Name <rahmanasmita24@gmail.comgit config --global user.name Your>
2026-05-04 22:07:48 +0600

    Update name in basic.py to include full name

--- basic.py
+++ basic.py
@@ -2,7 +2,7 @@ print("Welcome to Python Programming!")
 print("I am learning the basics.")
 
 
-name = "Asmita"
+name = "Asmita Rahman"
 age = 25
 
 print("My name is", name, "and I am", age, "years old.")
